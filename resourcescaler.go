99278ada3f1913bbbc039e142afd86cbad9fcd57
Hedingber <hedingber@gmail.com>
2019-10-23 01:24:29 +0300

    Supporting new scaler types structure (that supports multiple metrics per resource)

--- resourcescaler.go
+++ resourcescaler.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"k8s.io/apimachinery/pkg/types"
 	"os"
 	"time"
 
@@ -12,6 +11,7 @@ import (
 	"github.com/nuclio/zap"
 	"github.com/v3io/scaler-types"
 
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -49,9 +49,58 @@ func New(kubeconfigPath string, namespace string) (scaler_types.ResourceScaler, 
 
 func (s *AppResourceScaler) SetScale(resource scaler_types.Resource, scale int) error {
 	if scale == 0 {
-		return s.scaleServiceToZero(s.namespace, string(resource))
+		return s.scaleServiceToZero(s.namespace, resource.Name)
 	}
-	return s.scaleServiceFromZero(s.namespace, string(resource))
+	return s.scaleServiceFromZero(s.namespace, resource.Name)
+}
+
+func (s *AppResourceScaler) GetResources() ([]scaler_types.Resource, error) {
+	var iguazioTenantAppServicesSetMap map[string]interface{}
+	resources := make([]scaler_types.Resource, 0)
+
+	absPath := []string{"apis", "iguazio.com", "v1beta1", "namespaces", s.namespace, "iguaziotenantappservicesets", s.namespace}
+	iguazioTenantAppServicesSet, err := s.kubeClientSet.Discovery().RESTClient().Get().AbsPath(absPath...).Do().Raw()
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to get iguazio tenant app service sets")
+	}
+
+	if err := json.Unmarshal(iguazioTenantAppServicesSet, &iguazioTenantAppServicesSetMap); err != nil {
+		return nil, errors.Wrap(err, "Failed to unmarshal response")
+	}
+
+	statusServicesMap := s.parseStatusServices(iguazioTenantAppServicesSetMap)
+	specServicesMap := s.parseSpecServices(iguazioTenantAppServicesSetMap)
+
+	for statusServiceName, serviceStatus := range statusServicesMap {
+		stateString, err := s.parseServiceState(serviceStatus)
+		if err != nil {
+			s.logger.WarnWith("Failed parsing the service state, continuing", "err", err, "serviceStatus", serviceStatus)
+			continue
+		}
+
+		if stateString == "ready" {
+
+			scaleResources, err := s.parseScaleResources(specServicesMap[statusServiceName])
+			if err != nil {
+				s.logger.WarnWith("Failed parsing the scale resources, continuing", "err", err, "serviceSpec", specServicesMap[statusServiceName])
+				continue
+			}
+
+			resources = append(resources, scaler_types.Resource{
+				Name:           statusServiceName,
+				ScaleResources: scaleResources,
+			})
+		}
+	}
+
+	s.logger.DebugWith("Found services", "services", resources)
+
+	return resources, nil
+}
+
+func (s *AppResourceScaler) GetConfig() (*scaler_types.ResourceScalerConfig, error) {
+	return nil, nil
 }
 
 func (s *AppResourceScaler) scaleServiceFromZero(namespace string, serviceName string) error {
@@ -121,7 +170,7 @@ func (s *AppResourceScaler) waitForServiceReadiness(namespace string, serviceNam
 			return errors.Wrap(err, "Failed to get ready services")
 		}
 		for _, resource := range resourcesList {
-			if string(resource) == serviceName {
+			if resource.Name == serviceName {
 				s.logger.DebugWith("Service ready", "serviceName", serviceName)
 				return nil
 			}
@@ -131,64 +180,146 @@ func (s *AppResourceScaler) waitForServiceReadiness(namespace string, serviceNam
 	return nil
 }
 
-func (s *AppResourceScaler) GetResources() ([]scaler_types.Resource, error) {
-	var iguazioTenantAppServicesSetMap map[string]interface{}
-	resources := make([]scaler_types.Resource, 0)
-
-	absPath := []string{"apis", "iguazio.com", "v1beta1", "namespaces", s.namespace, "iguaziotenantappservicesets", s.namespace}
-	iguazioTenantAppServicesSet, err := s.kubeClientSet.Discovery().RESTClient().Get().AbsPath(absPath...).Do().Raw()
-
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to get iguazio tenant app service sets")
-	}
-
-	if err := json.Unmarshal(iguazioTenantAppServicesSet, &iguazioTenantAppServicesSetMap); err != nil {
-		return nil, errors.Wrap(err, "Failed to unmarshal response")
-	}
-
-	status, ok := iguazioTenantAppServicesSetMap["status"].(map[string]interface{})
-	if !ok {
-		s.logger.WarnWith("Service set does not have status", "serviceSet", iguazioTenantAppServicesSetMap)
-		return resources, nil
-	}
-
-	servicesMap, ok := status["services"].(map[string]interface{})
-	if !ok {
-		s.logger.WarnWith("Status does not have services", "status", status)
-		return resources, nil
-	}
-
-	for serviceName, serviceStatus := range servicesMap {
-		serviceStatusMap, ok := serviceStatus.(map[string]interface{})
-		if !ok {
-			s.logger.WarnWith("Service status type assertion failed, continuing", "serviceStatus", serviceStatus)
-			continue
-		}
-
-		stateString, ok := serviceStatusMap["state"].(string)
-		if !ok {
-			s.logger.WarnWith("Service status does not have state, continuing", "serviceStatusMap", serviceStatusMap)
-			continue
-		}
-
-		if stateString == "ready" {
-			resources = append(resources, scaler_types.Resource(serviceName))
-		}
-	}
-
-	s.logger.DebugWith("Found services", "services", resources)
-
-	return resources, nil
-}
-
-func (s *AppResourceScaler) GetConfig() (*scaler_types.ResourceScalerConfig, error) {
-	return nil, nil
-}
-
 func getClientConfig(kubeconfigPath string) (*rest.Config, error) {
 	if kubeconfigPath != "" {
 		return clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	}
 
 	return rest.InClusterConfig()
+}
+
+func (s *AppResourceScaler) parseSpecServices(iguazioTenantAppServicesSetMap map[string]interface{}) map[string]interface{} {
+	var servicesMap map[string]interface{}
+
+	spec, ok := iguazioTenantAppServicesSetMap["spec"].(map[string]interface{})
+	if !ok {
+		s.logger.WarnWith("Service set does not have spec", "serviceSet", iguazioTenantAppServicesSetMap)
+		return servicesMap
+	}
+
+	internalSpec, ok := spec["spec"].(map[string]interface{})
+	if !ok {
+		s.logger.WarnWith("Spec does not have internal spec", "spec", spec)
+		return servicesMap
+	}
+
+	tenants, ok := internalSpec["tenants"].([]interface{})
+	if !ok || len(tenants) != 1 {
+		s.logger.WarnWith("Internal spec does not have tenants or its length is invalid", "internalSpec", internalSpec)
+		return servicesMap
+	}
+
+	tenant, ok := tenants[0].(map[string]interface{})
+	if !ok {
+		s.logger.WarnWith("Tenant is not an object", "tenants", tenants)
+		return servicesMap
+	}
+
+	tenantSpec, ok := tenant["spec"].(map[string]interface{})
+	if !ok {
+		s.logger.WarnWith("Tenant does not have spec", "tenant", tenant)
+		return servicesMap
+	}
+
+	servicesMap, ok = tenantSpec["services"].(map[string]interface{})
+	if !ok {
+		s.logger.WarnWith("Tenant spec does not have services", "tenantSpec", tenantSpec)
+		return servicesMap
+	}
+
+	return servicesMap
+}
+
+func (s *AppResourceScaler) parseStatusServices(iguazioTenantAppServicesSetMap map[string]interface{}) map[string]interface{} {
+	var servicesMap map[string]interface{}
+	status, ok := iguazioTenantAppServicesSetMap["status"].(map[string]interface{})
+	if !ok {
+		s.logger.WarnWith("Service set does not have status", "serviceSet", iguazioTenantAppServicesSetMap)
+		return servicesMap
+	}
+
+	servicesMap, ok = status["services"].(map[string]interface{})
+	if !ok {
+		s.logger.WarnWith("Status does not have services", "status", status)
+		return servicesMap
+	}
+
+	return servicesMap
+}
+
+func (s *AppResourceScaler) parseServiceState(serviceStatus interface{}) (string, error) {
+	serviceStatusMap, ok := serviceStatus.(map[string]interface{})
+	if !ok {
+		return "", errors.New("Service status type assertion failed")
+	}
+
+	stateString, ok := serviceStatusMap["state"].(string)
+	if !ok {
+		return "", errors.New("Service status does not have state")
+	}
+
+	return stateString, nil
+}
+
+func (s *AppResourceScaler) parseScaleResources(serviceSpecInterface interface{}) ([]scaler_types.ScaleResource, error) {
+	var parsedScaleResources []scaler_types.ScaleResource
+	serviceSpec, ok := serviceSpecInterface.(map[string]interface{})
+	if !ok {
+		return []scaler_types.ScaleResource{}, errors.New("Service spec type assertion failed")
+	}
+
+	scaleToZeroSpec, ok := serviceSpec["scale_to_zero"].(map[string]interface{})
+	if !ok {
+		return []scaler_types.ScaleResource{}, errors.New("Service spec does not have scale to zero spec")
+	}
+
+	scaleToZeroMode, ok := scaleToZeroSpec["mode"].(string)
+	if !ok {
+		return []scaler_types.ScaleResource{}, errors.New("Scale to zero spec does not have mode")
+	}
+
+	// if it's not enabled there's no reason to parse the rest
+	if scaleToZeroMode == "enabled" {
+		scaleResourcesList, ok := scaleToZeroSpec["scaler_resources"].([]interface{})
+		if !ok {
+			return []scaler_types.ScaleResource{}, errors.New("Scale to zero spec does not have scale resources")
+		}
+
+		for _, scaleResourceInterface := range scaleResourcesList {
+			scaleResource, ok := scaleResourceInterface.(map[string]interface{})
+			if !ok {
+				return []scaler_types.ScaleResource{}, errors.New("Scale resource type assertion failed")
+			}
+
+			metricName, ok := scaleResource["metric_name"].(string)
+			if !ok {
+				return []scaler_types.ScaleResource{}, errors.New("Scale resource does not have metric name")
+			}
+
+			threshold, ok := scaleResource["threshold"].(int)
+			if !ok {
+				return []scaler_types.ScaleResource{}, errors.New("Scale resource does not have threshold")
+			}
+
+			windowSizeString, ok := scaleResource["window_size"].(string)
+			if !ok {
+				return []scaler_types.ScaleResource{}, errors.New("Scale resource does not have metric window size")
+			}
+
+			windowSize, err := time.ParseDuration(windowSizeString)
+			if err != nil {
+				return []scaler_types.ScaleResource{}, errors.Wrap(err, "Failed to parse window size")
+			}
+
+			parsedScaleResource := scaler_types.ScaleResource{
+				MetricName: metricName,
+				WindowSize: windowSize,
+				Threshold:  threshold,
+			}
+
+			parsedScaleResources = append(parsedScaleResources, parsedScaleResource)
+		}
+	}
+
+	return parsedScaleResources, nil
 }
